5ebd8fe95d78c793220a75c1ceb537f3d57ed3bb
kashior <kashior@me.com>
2017-06-07 19:07:48 +0300

    SPArrayList got new functions

--- SPArrayList.c
+++ SPArrayList.c
@@ -19,9 +19,49 @@ SPArrayList* spArrayListCreate(int maxSize){ //TODO what about initializing the 
 
 SPArrayList* spArrayListCopy(SPArrayList* src){
     SPArrayList* arr = malloc(src->maxSize*sizeof(int));
-    if (arr == NULL)
+    if (arr == NULL || src->elements==NULL)
         return NULL;
-    for (int i=0 ; i<sizeof(src) ; i++){
-        arr[i].
+    arr->elements=(int*)malloc(src->maxSize*sizeof(int));
+    for(int i=0; i<sizeof(src->elements)/sizeof(int); i++)
+        arr->elements[i]=src->elements[i];
+    arr->actualSize=src->actualSize;
+    arr->maxSize=src->maxSize;
+    return arr;
+}
+void spArrayListDestroy(SPArrayList* src)
+{
+    if(src!=NULL) {
+        free(src->elements);
+        free(src);
     }
+}
+SP_ARRAY_LIST_MESSAGE spArrayListClear(SPArrayList* src)
+{
+    SP_ARRAY_LIST_MESSAGE msg= SP_ARRAY_LIST_INVALID_ARGUMENT;
+    if(src==NULL)
+        return msg;
+    else
+    {
+        src->elements={};
+        src->actualSize=0;
+        msg = SP_ARRAY_LIST_SUCCESS;
+        return msg;
+    }
+
+}
+SP_ARRAY_LIST_MESSAGE spArrayListAddAt(SPArrayList* src, int elem, int index)
+{
+    if(src==NULL || index>src->actualSize || index<0)
+        return SP_ARRAY_LIST_INVALID_ARGUMENT;
+    if(src->actualSize==src->maxSize)
+        return SP_ARRAY_LIST_FULL;
+    src->actualSize++;
+    for(int i=src->actualSize;i>index;i--)
+        src->elements[i]=src->elements[i-1];
+    src->elements[index]=elem;
+    return SP_ARRAY_LIST_SUCCESS;
+}
+SP_ARRAY_LIST_MESSAGE spArrayListAddFirst(SPArrayList* src, int elem)
+{
+    return spArrayListAddAt( src,  elem, 0);
 }
--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include "SPFIARParser.h"
+#include "SPArrayList.h"
 
 int main() {
 
 // tests
 //    printf("%d\n",spParserIsInt("7"));
-    char string[1024];
-    fgets (string, 1024, stdin);
-    SPCommand com = spParserPraseLine(string);
-    printf("%d\n",com.cmd == SP_ADD_DISC);
-    printf("%d\n",com.arg);
-    printf("%d\n",com.validArg);
-
-    printf("%d\n",com.cmd == SP_INVALID_LINE);
+//    char string[1024];
+//    fgets (string, 1024, stdin);
+//    SPCommand com = spParserPraseLine(string);
+//    printf("%d\n",com.cmd == SP_ADD_DISC);
+//    printf("%d\n",com.arg);
+//    printf("%d\n",com.validArg);
+//
+//    printf("%d\n",com.cmd == SP_INVALID_LINE);
+    SPArrayList* sym=spArrayListCreate(20);
+    sym->elements[0]=3;
+    sym->elements[1]=4;
+    sym->elements[2]=5;
+    SPArrayList* syn=spArrayListCopy(sym);
 
 
 
