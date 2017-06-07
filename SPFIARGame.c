c67d164832e863a6207f9057d959642b09a1a171
uri <uri@t2ki.co.il>
2017-06-07 21:23:31 +0300

    progress on SPFIARGame

--- SPFIARGame.c
+++ SPFIARGame.c
@@ -34,6 +34,7 @@ SPFiarGame* spFiarGameCopy(SPFiarGame* src){
     copy->history = spArrayListCopy(src->history);
     if (copy->history == NULL)
         return NULL;
+    copy->undoCounter = src->undoCounter;
     return copy;
 }
 
@@ -41,5 +42,57 @@ void spFiarGameDestroy(SPFiarGame* src){
     if (src != NULL){
         free(src->history);
         free(src);
+    }
+}
+
+SP_FIAR_GAME_MESSAGE spFiarGameSetMove(SPFiarGame* src, int col){
+    if (src==NULL || col < 0 || col > SP_FIAR_GAME_N_COLUMNS-1)
+        return SP_FIAR_GAME_INVALID_ARGUMENT;
+    if (src->tops[col] == SP_FIAR_GAME_N_ROWS)
+        return SP_FIAR_GAME_INVALID_MOVE;
+    src->gameBoard[src->tops[col]][col] = src->currentPlayer;
+    src->tops[col]++;
+    if (src->currentPlayer == SP_FIAR_GAME_PLAYER_1_SYMBOL)
+        src->currentPlayer = SP_FIAR_GAME_PLAYER_2_SYMBOL;
+    else
+        src->currentPlayer = SP_FIAR_GAME_PLAYER_1_SYMBOL;
+    if (src->history->maxSize == src->history->actualSize)
+        spArrayListRemoveLast(src->history);
+    spArrayListAddFirst(src->history, col);
+    src->undoCounter = 0;
+    return SP_FIAR_GAME_SUCCESS;
+}
+
+bool spFiarGameIsValidMove(SPFiarGame* src, int col){
+    if (src==NULL || col < 0 || col > SP_FIAR_GAME_N_COLUMNS-1)
+        return false;
+    if (src->tops[col] == SP_FIAR_GAME_N_ROWS)
+        return false;
+    return true;
+}
+
+SP_FIAR_GAME_MESSAGE spFiarGameUndoPrevMove(SPFiarGame* src){
+    if (src==NULL)
+        return SP_FIAR_GAME_INVALID_ARGUMENT;
+    if (src->undoCounter==src->history->maxSize || src->history->actualSize==0)
+        return SP_FIAR_GAME_NO_HISTORY;
+
+}
+
+
+char spFiarGameGetCurrentPlayer(SPFiarGame* src){
+    if (src==NULL)
+        return SP_FIAR_GAME_EMPTY_ENTRY;
+    return src->currentPlayer;
+}
+
+char spFiarCheckWinner(SPFiarGame* src){
+    int player1Counter = 0;
+    int player2Counter = 0;
+    for (int i=0 ; i< SP_FIAR_GAME_N_ROWS-SP_FIAR_GAME_SPAN +1; i++){
+//        for (int j = i ; j< i+SP_FIAR_GAME_SPAN; j++){
+//            if (src->gameBoard[i][j] != src->gameBoard[i][j+1] )
+//        }
+
     }
 }
